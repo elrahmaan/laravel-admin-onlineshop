d093507b7dfa925e9b32ca765133cf472d67f44f
Abdul Rahman Saleh <abdulrahmansaleh152@gmail.com>
2021-10-11 16:03:02 +0700

    create models and controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\OrderController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +17,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('master.app');
-});
+// Route::get('/', function () {
+//     return view('master.app');
+// });
+Route::get('/home', [DashboardController::class, 'index'])->name('home');
+Route::get('/user', [UserController::class, 'index'])->name('user');
+Route::get('/category', [CategoryController::class, 'index'])->name('category');
+Route::get('/product', [ProductController::class, 'index'])->name('product');
+Route::get('/order', [OrderController::class, 'index'])->name('order');
+
+Route::resource('user', UserController::class)->only(['index']);
+Route::resource('category', CategoryController::class)->only(['index']);
+Route::resource('product', ProductController::class)->only(['index']);
+Route::resource('order', OrderController::class)->only(['index']);
