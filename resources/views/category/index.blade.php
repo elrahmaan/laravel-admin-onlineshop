54feba8e1f3d72ee16bad0f15b94cae278a1f64c
Abdul Rahman Saleh <abdulrahmansaleh152@gmail.com>
2021-10-20 22:42:03 +0700

    change modal

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -51,31 +51,31 @@ Halaman Data Kategori Barang
                                 <td>{{$cat['category_name']}}</td>
                                 <td>{{$cat['category_desc']}}</td>
                                 <td align="center">
-                                    <a href="#" class="btn btn-info" style="width:35px;" data-toggle="modal" id="showCategory" data-target="#categoryModal" data-code="{{$cat['category_code']}}" data-name="{{$cat['category_name']}}" data-image="{{$cat['category_icon']}}" data-desc="{{$cat['category_desc']}}"><i class="ik ik-eye"></i></a>
-                                    <div class="modal fade" id="categoryModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongLabel" aria-hidden="true">
-                                        <div class="modal-dialog" role="document">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <h5 class="modal-title" id="exampleModalLongLabel">Data Kategori</h5>
-                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                                                </div>
-                                                <div class="modal-body">
-                                                    <!-- modal content -->
-                                                    <p id="categoryCode"></p>
-                                                    <p id="categoryName"></p>
-                                                    <img src="" id="categoryIcon">
-                                                    <p id="categoryClass"></p>
-                                                </div>
-                                                <div class="modal-footer">
-                                                    <button type="button" class="btn btn-primary" data-dismiss="modal">OK</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    <a href="#" class="btn btn-info" style="width:35px;" data-toggle="modal" id="showCategory" data-target="#categoryModal{{$cat->id()}}"><i class="ik ik-eye"></i></a>
                                     <a href="/category/{{$cat->id()}}/edit"><button type="button" class="btn btn-warning" style="background-color:#ffc107; border:none; width:35px;"><i class="ik ik-edit iconT"></i></button></a>
                                     <a href="/category/{{$cat->id()}}/delete"><button type="button" class="btn btn-danger" style="width:35px;"><i class="ik ik-trash-2 iconT"></i></button></a>
                                 </td>
                             </tr>
+                            <div class="modal fade" id="categoryModal{{$cat->id()}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLongLabel">Data Kategori</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <!-- modal content -->
+                                            <p>{{$cat['category_code']}}</p>
+                                            <p>{{$cat['category_name']}}</p>
+                                            <img src="{{asset($cat['category_icon'])}}">
+                                            <p>{{$cat['category_desc']}}</p>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-primary" data-dismiss="modal">OK</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                             @endforeach
                         </tbody>
                     </table>
@@ -87,21 +87,6 @@ Halaman Data Kategori Barang
 
 @endsection
 @section('script')
-<script type="text/javascript">
-    $(document).on('click', '#showCategory', function (event) {
-        let category_code = $(this).attr('data-code');
-        let category_name = $(this).attr('data-name');
-        let category_icon = $(this).attr('data-icon');
-        let category_desc = $(this).attr('data-desc');
-
-        // set nilai
-        document.getElementById('categoryCode').innerHTML = category_code;
-        document.getElementById('categoryName').innerHTML = category_name;
-        $('#categoryIcon').attr('src', 'http://127.0.0.1:8000/' + category_icon);
-        document.getElementById('categoryDesc').innerHTML = category_desc;
-    });
-
-</script>
 @endsection
 @section('fixedButton')
 <a class="fixedButtonRefresh">
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -58,35 +58,7 @@ Halaman Data Barang
                                 <td>{{$product['product_stock']}}</td>
                                 <td align="center">
                                     <a href="#" data-productName="{{$product['product_name']}}" id="showProduct" class="btn btn-info" style="width:35px;"
-                                            data-toggle="modal" data-target="#productModal" data-code="{{$product['product_code']}}" data-name="{{$product['product_name']}}" data-category="{{$category['category_name']}}" data-image="{{$product['product_image']}}" data-price="IDR {{$product['product_price']}}" data-desc="{{$product['product_desc']}}" data-stock="{{$product['product_stock']}}"><i class="ik ik-eye"></i></a>
-                                            <!-- modal -->
-                                    <div class="modal fade" id="productModal" tabindex="-1" role="dialog"
-                                        aria-labelledby="exampleModalLongLabel" aria-hidden="true">
-                                        <div class="modal-dialog" role="document">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <h5 class="modal-title" id="exampleModalLongLabel">Data barang</h5>
-                                                    <button type="button" class="close" data-dismiss="modal"
-                                                        aria-label="Close"><span
-                                                            aria-hidden="true">&times;</span></button>
-                                                </div>
-                                                <div class="modal-body">
-                                                    <!-- modal content -->
-                                                    <p id="productCode"></p>
-                                                    <p id="productName"></p>
-                                                    <img src="" id="productImage">
-                                                    <p id="productCategory"></p>
-                                                    <p id="productPrice"></p>
-                                                    <p id="productDesc"></p>
-                                                    <p id="productStock"></p>
-                                                </div>
-                                                <div class="modal-footer">
-                                                    <button type="button" class="btn btn-primary"
-                                                        data-dismiss="modal">OK</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                            data-toggle="modal" data-target="#productModal{{$product->id()}}"><i class="ik ik-eye"></i></a>
                                     <a href="/product/{{$product->id()}}/edit"><button type="button"
                                             class="btn btn-warning"
                                             style="background-color:#ffc107; border:none; width:35px;"><i
@@ -96,6 +68,30 @@ Halaman Data Barang
                                                 class="ik ik-trash-2 iconT"></i></button></a>
                                 </td>
                             </tr>
+                            <!-- modal -->
+                            <div class="modal fade" id="productModal{{$product->id()}}" tabindex="-1" role="dialog"
+                                aria-labelledby="exampleModalLongLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLongLabel">Data barang</h5>
+                                            <button type="button" class="close" data-dismiss="modal"
+                                                aria-label="Close"><span
+                                                    aria-hidden="true">&times;</span></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <!-- modal content -->
+                                            <p>{{$product['product_code']}}</p>
+                                            <p>{{$product['product_name']}}</p>
+                                            <p><img src="{{asset($product['product_image'])}}"></p>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-primary"
+                                                data-dismiss="modal">OK</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                             @endif
                             @endforeach
                             @endforeach
@@ -106,31 +102,8 @@ Halaman Data Barang
         </div>
     </div>
 </div>
-@section('script')
-<script type="text/javascript">
-    $(document).on('click', '#showProduct', function (event) {
-        let product_code = $(this).attr('data-code');
-        let product_name = $(this).attr('data-name');
-        let product_image = $(this).attr('data-image');
-        let product_category = $(this).attr('data-category');
-        let product_price = $(this).attr('data-price');
-        let product_desc = $(this).attr('data-desc');
-        let product_stock = $(this).attr('data-stock');
-
-        // set nilai
-        document.getElementById('productCode').innerHTML = product_code;
-        document.getElementById('productName').innerHTML = product_name;
-        $('#productImage').attr('src', 'http://127.0.0.1:8000/' + product_image);
-        document.getElementById('productCategory').innerHTML = product_category;
-        document.getElementById('productPrice').innerHTML = product_price;
-        document.getElementById('productDesc').innerHTML = product_desc;
-        document.getElementById('productStock').innerHTML = product_stock;
-    });
-
-</script>
 @endsection
 
-@endsection
 @section('fixedButton')
 <a class="fixedButtonRefresh">
     <button data-toggle="tooltip" data-placement="top" title="" type="" class="btn btn-icon btn-secondary "
